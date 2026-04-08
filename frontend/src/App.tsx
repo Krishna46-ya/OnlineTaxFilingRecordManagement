342d2edc269ab931344a3969123af6f2ee8e6119
KrishnaYad <krishnayadav46846@gmail.com>
2026-04-08 08:35:19 +0530

    fill: cloude frontend added for review

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Signup } from './pages/signup'
+import { Signin } from './pages/signin'
+import SignupForm from './pages/SignupForm'
 
 function App() {
 
-  return (
-    <>
-     <div>KRISHNA</div>
-    </>
-  )
+    return (
+        <>
+            <BrowserRouter>
+            <Routes>
+                <Route element={<Signup></Signup>} path='/signup'></Route>
+                <Route element={<Signin></Signin>} path='/signin'></Route>
+                <Route element={<SignupForm></SignupForm>} path='/'></Route>
+            </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App
